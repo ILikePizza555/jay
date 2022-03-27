94b81c0ed33f3d46f93aed1420e7cfd8ff2ddfb7
Izzy Lancaster <avrisaac555@gmail.com>
2022-03-27 16:58:10 -0700

    First commit!

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Jay is a web-based smart home inventory system. Named after California Scrub Jays";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {self, nixpkgs, flake-utils, ...}:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        commonBuildInputs = [
+          pkgs.nodejs-16_x
+        ];
+      in
+        {
+          defaultPackages = pkgs.stdenv.mkDerivation {
+            pname = "jay";
+            version = "0.1.0";
+            src = ./.;
+            buildInputs = commonBuildInputs;
+          };
+
+          devShell = pkgs.mkShell {
+            packages = commonBuildInputs;
+          };
+        }
+    );
+}
