a0845aa266e3360855c9f43131ba4ac07a98ce20
Izzy Lancaster <avrisaac555@gmail.com>
2022-03-27 18:23:09 -0700

    Fix cli commands

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,26 @@ import { Command } from "commander";
 
 const cli = new Command();
 
-cli.command("create")
+const createCommand = cli.command("create")
     .option("-d, --description <description>", "A description of the object.")
-    .option("-t, --type <type>", "The type or category of the object being created.")
-    .command("container")
-        .description("Creates a new container.")
-        .argument("<name>", "The name of the container.")
-        .argument("[location]", "The location of the container. Must either be a container name or id.")
-    .command("item")
-        .description("Creates a new item.")
-        .argument("<name>", "The name of the item.")
-        .argument("<location>", "The location of the item. Must either be a container name or id.")
-        .argument("[quantity]", "Quantity of the item. Must be greater than zero.", 1);
+    .option("-t, --type <type>", "The type or category of the object being created.");
+
+createCommand.command("container")
+    .description("Creates a new container.")
+    .argument("<name>", "The name of the container.")
+    .argument("[location]", "The location of the container. Must either be a container name or id.");
+createCommand.command("item")
+    .description("Creates a new item.")
+    .argument("<name>", "The name of the item.")
+    .argument("<location>", "The location of the item. Must either be a container name or id.")
+    .argument("[quantity]", "Quantity of the item. Must be greater than zero.", 1);
     
-cli.command("list")
-    .command("all")
-        .description("Lists all the objects in the catalogue.")
-    .command("container")
-        .description("Lists all items in the specified container.")
-        .argument("<name-or-id>", "The name or id of a container.");
+const listCommand = cli.command("list");
+listCommand.command("all")
+    .description("Lists all the objects in the catalogue.");
+listCommand.command("container")
+    .description("Lists all items in the specified container.")
+    .argument("<name-or-id>", "The name or id of a container.");
 
 cli.command("delete")
     .description("Deletes an object.")
