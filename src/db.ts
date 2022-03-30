bfc2df8e50f6e606404b670935c56aca5326bef6
Izzy Lancaster <avrisaac555@gmail.com>
2022-03-29 22:47:10 -0700

    Fixed getContainerBy* Statements

--- src/db.ts
+++ src/db.ts
@@ -43,11 +43,11 @@ export class DatabaseConnection {
         );
         
         this.getContainerByUuidStatement = this._db.prepare(
-            `SELECT * FROM ${CONTAINERS_TABLE_NAME} WHERE uuid = @uuid;`
+            `SELECT * FROM ${CONTAINERS_TABLE_NAME} WHERE uuid = ?;`
         );
 
         this.getContainerByNameStatement = this._db.prepare(
-            `SELECT * FROM ${CONTAINERS_TABLE_NAME} WHERE name = @name;`
+            `SELECT * FROM ${CONTAINERS_TABLE_NAME} WHERE name = ?;`
         );
 
         this.selectAllStatement = this._db.prepare(
