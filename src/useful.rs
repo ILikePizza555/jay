df8cb67a6fc18266d3efc8b93417318a1980552e
Izzy Lancaster <avrisaac555@gmail.com>
2022-05-15 13:55:33 -0700

    Changed default None display string to "-"

--- src/useful.rs
+++ src/useful.rs
@@ -11,7 +11,7 @@ pub fn format_table_uuid(uuid: &Uuid) -> String {
 /// Converts an `Option` into a `String`.
 /// This is primarily used by Tabled trait implementations for models.
 pub fn format_table_option<T>(opt: &Option<T>, op: impl FnOnce(&T) -> String) -> String {
-    opt.as_ref().map(op).unwrap_or(String::from("None"))
+    opt.as_ref().map(op).unwrap_or(String::from("-"))
 }
 
 /// Converts an `Option` containing a type that implements `Display` into a `String`.
